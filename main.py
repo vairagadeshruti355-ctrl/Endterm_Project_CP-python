f2b4094cc749f32fcc611ff64640ec24314a381c
vairagadeshruti355-ctrl <vairagadeshruti355@gmail.com>
2026-07-22 12:07:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from maze_generator import generate_maze
+from graph import create_graph
+from pathfinder import shortest_path
 
+maze = generate_maze()
+
+print("Maze\n")
+
+for row in maze:
+    print(row)
+
+graph = create_graph(maze)
+
+path,cost,monsters = shortest_path(graph,maze)
+
+print("\nShortest Path")
+
+for node in path:
+    print(node)
+
+print("\nMinimum Cost :",cost)
+print("Monsters Encountered :",monsters)
