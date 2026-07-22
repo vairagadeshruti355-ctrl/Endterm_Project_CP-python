4672d154d61f8a99766dfe0217418afba6484a64
vairagadeshruti355-ctrl <vairagadeshruti355@gmail.com>
2026-07-22 12:06:12 +0530

    Update maze_generator.py

--- maze_generator.py
+++ maze_generator.py
@@ -1,1 +1,21 @@
+import random
 
+cells = [".", ".", ".", "#", "T", "M"]
+
+def generate_maze():
+    cols = random.randint(3, 10)
+    maze = []
+
+    for i in range(cols):
+        if i == 0:
+            row = "S" + "".join(random.choice(cells) for _ in range(cols - 1))
+
+        elif i == cols - 1:
+            row = "".join(random.choice(cells) for _ in range(cols - 1)) + "E"
+
+        else:
+            row = "".join(random.choice(cells) for _ in range(cols))
+
+        maze.append(row)
+
+    return maze
